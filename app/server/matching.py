72629ceed63f4cde208240439dd4f88ec83baac3
PierreDerathe <pierre.derathe@universite-paris-saclay.fr>
2024-07-19 11:37:39 +0200

    chore: small fix of the API
    * change the format of the output
    * The API can be run from any directory

--- app/server/matching.py
+++ app/server/matching.py
@@ -9,7 +9,8 @@ from flask_cors import CORS
 from inference.linker import EntityLinker
 
 # Add the parent and the current directory to the path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 app = Flask(__name__)
@@ -18,12 +19,12 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Load occupations data
 try:
-    dict_occupations = pd.read_csv("occupations_en.csv", sep=",", header=0)
+    dict_occupations = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname(__file__), "occupations_en.csv")), sep=",", header=0)
 except FileNotFoundError:
     print("Error: occupations_en.csv file not found.")
     sys.exit(1)
 
-
+custom_pipeline = EntityLinker(entity_model='tabiya/bert-base-job-extract', similarity_model='all-MiniLM-L6-v2', output_format='esco_code', k=10)
 # preferredLabel
 # code
 
@@ -39,7 +40,6 @@ def match():
     if not job_descr:
         return jsonify({"error": "job_descr is required"}), 400
 
-    custom_pipeline = EntityLinker(entity_model='tabiya/bert-base-job-extract', similarity_model='all-MiniLM-L6-v2')
     extracted = custom_pipeline(job_descr)
     if not extracted:
         return jsonify({"error": "No entities extracted"}), 400
